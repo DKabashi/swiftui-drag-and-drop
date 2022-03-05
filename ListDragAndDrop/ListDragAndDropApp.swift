9205125aca5218a363fd3310537f6c2a1861836e
Donat Kabashi <donatkabashi@gmail.com>
2022-03-05 20:10:32 +0100

    Initial Commit

---
+++ ListDragAndDrop/ListDragAndDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListDragAndDropApp.swift
+//  ListDragAndDrop
+//
+//  Created by Donat Kabashi on 5.3.22.
+//
+
+import SwiftUI
+
+@main
+struct ListDragAndDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
